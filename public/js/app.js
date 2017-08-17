38516242fa8c348c3a1ce79198fbbe385315379a
Rachel Chhay <rachelchhay@Rachels-MacBook-Pro.local>
2017-08-17 16:25:26 -0500

    Added controller and fixed index.html script tags

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+const app = angular.module('FoodApp', []);
+
+app.controller('FoodController', ['$http', function($http){
+
+
+
+
+
+
+
+
+
+// end of controller
+}]);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const bodyParser = require('body-Parser');
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
