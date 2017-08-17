b5897eca1ba90e5e3613b39447d764317bd2ed00
Rachel Chhay <rachelchhay@Rachels-MacBook-Pro.local>
2017-08-17 17:39:09 -0500

    Create and Get food functions made in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,41 @@
 const app = angular.module('FoodApp', []);
 
 app.controller('FoodController', ['$http', function($http){
+  // redefine "this"
+  const controller = this;
 
+  // Create Food function
+  this.createFood = function() {
+    $http({
+      method: 'POST',
+      url: '/food',
+      data: {
+        name: this.name,
+        sugar: this.sugar,
+        calories: this.calories,
+        carbs: this.carbs,
+        fiber: this.fiber,
+        type: this.type
+      }
+    }).then(function(response){
+      controller.getFood();
+    }, function(error){
+      console.log(error);
+    });
+  }
 
+  this.getFood = function(){
+    $http({
+      method: 'GET',
+      url: '/food'
+    }).then(function(response){
+      controller.foundFood = response.data
+    }, function(error){
+      console.log(error);
+    });
+  }
+
+  this.getFood();
 
 
 
